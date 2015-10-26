cc0b764761bd1e2b47e4c81b1dbcd60e40abe8e3
1400003 <1400003@rgu.ac.uk>
2015-10-26 17:43:46 +0000

    Just for the lab

--- helloSelector.php
+++ helloSelector.php
@@ -4,7 +4,7 @@
     </head>
     <body>
         <h1> Where would you like to say hello? </h1><br><hr><br>
-        <a href= "http://darkgtr.azurewebsites.net/helloPrinter.php?planet=earth">Earth</a><br>
-        <a href= "http://darkgtr.azurewebsites.net/helloPrinter.php?planet=mars">Mars</a><br>
-        <a href= "http://darkgtr.azurewebsites.net/helloPrinter.php?planet=uranus">Uranus</a>
+        <a href= "http://darkgtr.azurewebsites.net/helloPrinter.php?planet=Earth">Earth</a><br>
+        <a href= "http://darkgtr.azurewebsites.net/helloPrinter.php?planet=Mars">Mars</a><br>
+        <a href= "http://darkgtr.azurewebsites.net/helloPrinter.php?planet=Uranus">Uranus</a>
     </body>
