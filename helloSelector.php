9fd4b1821bbdf006556bf568edc608d758b68f61
1400003 <1400003@rgu.ac.uk>
2015-10-27 15:56:18 +0000

    Just for the lab

--- helloSelector.php
+++ helloSelector.php
@@ -7,5 +7,13 @@
         <h1> Where would you like to say hello? </h1><br><hr><br>
         <a href= "http://darkgtr.azurewebsites.net/helloPrinter.php?planet=Earth">Earth</a><br>
         <a href= "http://darkgtr.azurewebsites.net/helloPrinter.php?planet=Mars">Mars</a><br>
-        <a href= "http://darkgtr.azurewebsites.net/helloPrinter.php?planet=Uranus">Uranus</a>
+        <a href= "http://darkgtr.azurewebsites.net/helloPrinter.php?planet=Uranus">Uranus</a><br><hr><br>
+        <div>
+            <?php
+                $myarray = array("no products", "specs", "mugs", "specs and sausage rolls");
+                for ($i = 1; $i < 31; $i++) {
+                    ECHO "on the" . $i . " of the month" . $myarray . " are available<br>";
+                }
+            ?>
+        </div>
     </body>
