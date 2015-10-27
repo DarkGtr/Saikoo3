85b5f22fea8e233941c93b2e377d81b603f56fd4
1400003 <1400003@rgu.ac.uk>
2015-10-27 15:04:42 +0000

    Just for the lab

--- helloSelector.php
+++ helloSelector.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
     <head>
+        <meta charset="UTF-8">
         <title>helloSelector</title>
     </head>
     <body>
@@ -7,4 +8,18 @@
         <a href= "http://darkgtr.azurewebsites.net/helloPrinter.php?planet=Earth">Earth</a><br>
         <a href= "http://darkgtr.azurewebsites.net/helloPrinter.php?planet=Mars">Mars</a><br>
         <a href= "http://darkgtr.azurewebsites.net/helloPrinter.php?planet=Uranus">Uranus</a>
+        <?php
+            $x  = 0;
+            while($x < 31){
+                $x++;
+                if($x / 2){
+                    echo "on the " . $x . "st of the month/2";
+                }elseif($x / 3){
+                    echo "on the " . $x . "st of the month/3";
+                }elseif($x / 4){
+                    echo "on the " . $x . "st of the month/4";
+                }
+
+            }
+        ?>
     </body>
