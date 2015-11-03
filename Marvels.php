95ceec34304fba828028fb77a014816a4048f727
1400003 <1400003@rgu.ac.uk>
2015-11-03 09:58:45 +0000

    hey

--- Marvels.php
+++ Marvels.php
@@ -44,7 +44,7 @@
         }
 
         if($_POST ["search"] == "Oscar"){
-        $search = "SELECT * FROM marvelmovies WHERE notes LIKE '%Oscar^%'";
+        $search = "SELECT * FROM marvelmovies WHERE notes LIKE '%Oscar%'";
         $result = $db->query($search);
         WHILE ($row = $result->fetch_array())
             echo  "<p>" . $row['title'] . "</p>";
